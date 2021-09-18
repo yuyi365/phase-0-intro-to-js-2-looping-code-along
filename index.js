3ebf9e1b7be227b031e2a6b504b04771a641fd3e
Yuyi <yuyi365@gmail.com>
2021-09-18 17:09:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Jane", "Jake", "Julia"];
+const event = "birthday";
+let newArray = [];
+
+function writeCards (names,event) {
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+    debugger;
+}
+
+function countDown (){
+    let count = 10;
+    while (count >= 0) {
+        console.log(count);
+        count--
+    }
+    return count;
+}
